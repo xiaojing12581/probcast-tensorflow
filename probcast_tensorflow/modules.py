46613947ad90f9eb3c54fb24e6eed79cb329b1f3
xiaojing12581 <110285117+xiaojing12581@users.noreply.github.com>
2023-06-14 15:47:56 +0800

    Update modules.py

--- probcast_tensorflow/modules.py
+++ probcast_tensorflow/modules.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+#生成器
 def generator(gru_units, dense_units, sequence_length, noise_dimension, model_dimension):
 
     '''
@@ -8,40 +8,40 @@ def generator(gru_units, dense_units, sequence_length, noise_dimension, model_di
     Parameters:
     __________________________________
     gru_units: list.
-        Number of hidden units of each GRU layer.
+        Number of hidden units of each GRU layer.每个GRU层的隐藏单元数
 
     dense_units: int.
-        Number of hidden units of the dense layer.
+        Number of hidden units of the dense layer.密集层的隐藏单元数
 
     sequence_length: int.
-        Number of past time steps used as input.
+        Number of past time steps used as input.用作输入的过去时间步数
 
     noise_dimension: int.
-        Dimension of the noise vector concatenated to the outputs of the GRU block.
+        Dimension of the noise vector concatenated to the outputs of the GRU block.连接到GRU模块输出的噪声向量的维数
 
     model_dimension: int.
-        Number of time series.
+        Number of time series.时间序列的数量
     '''
     
-    # Inputs.
+    # Inputs.用于构建网络得第一层（输入层）
     inputs = tf.keras.layers.Input(shape=(sequence_length, model_dimension))
 
-    # GRU block.
+    # GRU block.门限循环单元网络
     outputs = tf.keras.layers.GRU(units=gru_units[0], return_sequences=False if len(gru_units) == 1 else True)(inputs)
     for i in range(1, len(gru_units)):
         outputs = tf.keras.layers.GRU(units=gru_units[i], return_sequences=True if i < len(gru_units) - 1 else False)(outputs)
 
     # Noise vector.
     noise = tf.keras.layers.Input(shape=noise_dimension)
-    outputs = tf.keras.layers.Concatenate(axis=-1)([noise, outputs])
+    outputs = tf.keras.layers.Concatenate(axis=-1)([noise, outputs])#指定维度拼接
 
-    # Dense layers.
+    # Dense layers.全连接层（特征提取器）
     outputs = tf.keras.layers.Dense(units=dense_units)(outputs)
     outputs = tf.keras.layers.Dense(units=model_dimension)(outputs)
 
-    return tf.keras.models.Model([inputs, noise], outputs)
+    return tf.keras.models.Model([inputs, noise], outputs)#创建模型
 
-
+#鉴别器
 def discriminator(gru_units, dense_units, sequence_length, model_dimension):
 
     '''
